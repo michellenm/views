f82ee1b95bda5337f6a37c37ac10b2a7be799654
php 24 <php.24@daphne.cec.ime.usp.br>
2018-01-31 22:40:11 -0200

    Alteração view by prof

--- app/Http/Controllers/CursosController.php
+++ app/Http/Controllers/CursosController.php
@@ -6,9 +6,16 @@ use Illuminate\Http\Request;
 
 class CursosController extends Controller
 {
+  private  $cursos = ['TDD', 'Front End', 'Java','Scrum'];// atributo private para poder usar 'cursos' em td pgm
     // action = metodo que trata uma reqisição
     public function index(){
-      $cursos = ['TDD', 'Front End', 'Java','Scrum'];
-      return view ('cursos', compact('cursos'));
+      $cursos = $this->cursos;
+      return view ('cursos.index', compact('cursos'));
+    }
+
+    public function show($x){ // x é a posicao do array
+      $curso = $this->cursos[$x];
+      return View('cursos.show', compact('curso'));// poderia ser cursos/show
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 Route::get('/ ola', function () { //get é uma requisição do tipo que assim q digitasa devolve uma pagina
                               // '/' = usuario digita /na barra do  navegador
                               // function = vai executar a funão
@@ -10,13 +8,15 @@ $nome = "Mimi, amor da minha vida !!!!!";
                             // return = retorna algo
                             // view  = é nao uma pagina, e deve ser criada na pasta views
 });
-
 /*Route::get('/cursos', function(){
 $cursos = ['TDD', 'Front End', 'Java','Scrum'];
-
 return view('cursos', compact ('cursos'));
-
-
 });*/
 
 Route::get('/cursos', 'CursosController@index');
+
+// exercicio 11
+// os mones das classes e metodos devem ser iguais ex:Cursos
+// entre colchete coloca o id ... ex x
+// /cursos/1 = x= TDD
+Route::get('/cursos/{x}' , 'CursosController@show');
